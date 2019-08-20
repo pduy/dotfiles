bf401f5a3e2a2340a187ec02ff93991f21ce78b4
Duy Pham <pham.duy89@gmail.com>
2019-08-20 23:36:53 +0200

    update border color

--- .i3/fuzzy_lock.sh
+++ .i3/fuzzy_lock.sh
@@ -3,7 +3,7 @@ scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
 #mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 0x6 -scale 25% -scale 400% /tmp/screen_locked.png
+convert /tmp/screen_locked.png -blur 0x8 -scale 25% -scale 400% /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
