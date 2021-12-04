58c783f3c7d79fb66df4876850d3731ac7e97f23
Duy Pham <duy.pham@zalando.de>
2021-12-04 23:44:14 +0100

    update swap desktop

--- sxhkd/swap_desktop.sh
+++ sxhkd/swap_desktop.sh
@@ -3,8 +3,7 @@
 ws="$(bspc query -D -d --names)"
 bspc desktop -s $1 &&\
 bspc desktop -n temp &&\
-bspc desktop -f $ws &&\
-bspc desktop -n $1 &&\
+bspc desktop -f $1 &&\
+bspc desktop -n $ws &&\
 bspc desktop -f temp &&\
-bspc desktop -n $ws
-
+bspc desktop -n $1
